ecb9698e14587ae4625725bb15917824a264efd9
fasta <fastakovskiy.mihail04@gmail.com>
2024-12-05 21:42:17 +0200

    Добавьте файлы проекта.

---
+++ NetCsharpGenT2/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetCsharpGenT2;
+
+//task1
+
+//Account account = new Account();
+//account.AddInfo("Alex333", "12345678");
+//account.AddInfo("Oleg22", "1234");
+//account.AddInfo("Dima3456", "67899");
+//foreach(var item in account.info)
+//{
+//    Console.WriteLine(item.Key +" - "+item.Value);
+//}
+//Console.WriteLine();
+
+//account.RemoveInfo("Alex333");
+
+//foreach (var item in account.info)
+//{
+//    Console.WriteLine(item.Key + " - " + item.Value);
+//}
+//Console.WriteLine();
+
+//account.RedInfo("Oleg22", "Oleg333", "Ol12345678");
+//foreach (var item in account.info)
+//{
+//    Console.WriteLine(item.Key + " - " + item.Value);
+//}
+//Console.WriteLine();
+
+//Console.WriteLine(account.GetPassword("Oleg333"));
+
+
+//task2
+
+//Dictionary d1 = new Dictionary();
+//d1.AddWord("beautiful", "belle");
+//d1.AddWord("clever", "intelligent");
+//d1.AddWord("mad", "furieux");
+//foreach (var item in d1.dict)
+//{
+//    Console.Write(item.Key + " - " + string.Join(", ", item.Value));
+
+//}
+//d1.AddTranslate("beautiful", "superbe");
+//foreach (var item in d1.dict)
+//{
+//    Console.Write(item.Key + " - "+string.Join(", ", item.Value));
+   
+//}
+//Console.WriteLine();
+//d1.Remove("mad");
+//foreach (var item in d1.dict)
+//{
+//    Console.Write(item.Key + " - " + string.Join(", ", item.Value));
+
+//}
+//d1.RemoveTranslate("clever", "intelligent");
+//foreach (var item in d1.dict)
+//{
+//    Console.Write(item.Key + " - " + string.Join(", ", item.Value));
+
+//}
+//d1.EditWord("clever", "clever", "intelligent");
+//foreach (var item in d1.dict)
+//{
+//    Console.Write(item.Key + " - " + string.Join(", ", item.Value));
+
+//}
+
+//d1.EditTranslate("beautiful", "belle", "magnifique");
+//foreach (var item in d1.dict)
+//{
+//    Console.Write(item.Key + " - " + string.Join(", ", item.Value));
+
+//}
+
+//d1.FindTranslate("clever");
+//task3
+Person p1 = new Person("Alex");
+Person p2 = new Person("Luis");
+Person p3 = new Person("Olga", "12:30");
+CafeQueue cq1 = new CafeQueue();
+cq1.AddToQueue(p1);
+cq1.AddToQueue(p2);
+cq1.ShowQueue();
+Console.WriteLine();
+cq1.AddToQueue(p3);
+cq1.ShowQueue();
+Console.WriteLine();
+cq1.FreeTable();
+cq1.ShowQueue();
+Console.WriteLine();
---
+++ NetCsharpGenT2/classes.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCsharpGenT2
+{
+    internal class Account
+    {
+        public Dictionary<string, string> info;
+        
+        public Account() { 
+        info=new Dictionary<string, string>();
+           
+        }
+
+        public void AddInfo(string login, string pass)
+        {
+            info.Add(login, pass);
+            
+        }
+        public void RemoveInfo (string login)
+        {
+            info.Remove(login);
+        }
+        public void RedInfo(string login,string new_login, string new_pass)
+        {
+            RemoveInfo(login);
+            AddInfo(new_login, new_pass);
+        }
+        public string GetPassword(string login)
+        {
+            return info[login];
+        }
+    }
+
+    public class Dictionary
+    {
+        public Dictionary<string, List<string>> dict;
+
+        public Dictionary()
+        {
+            dict = new Dictionary<string, List<string>>();
+        }
+
+        public void AddWord(string w, string t)
+        {
+            dict.Add(w, new List<string> { t });
+        }
+        public void AddTranslate(string w, string t)
+        {
+            dict[w].Add(t);
+        }
+        public void Remove(string w)
+        {
+            dict.Remove(w);
+        }
+        public void RemoveTranslate(string w, string trans)
+        {
+            dict[w].Remove(trans);
+        }
+        public void EditWord(string w, string new_w, string new_trans)
+        {
+            Remove(w);
+            AddWord(new_w, new_trans);
+        }
+        public void EditTranslate(string w, string trans, string new_trans)
+        {
+            RemoveTranslate(w, trans);
+            AddTranslate(w, new_trans);
+        }
+        public List<string> FindTranslate(string w)
+        {
+            return dict[w];
+        }
+    }
+     public class Person
+    {
+        public string Name { get; set; }
+        public string Time { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+            Time = null;
+        }
+        public Person(string name, string time): this(name)
+        {
+            Time = time;
+        }
+    }
+    internal class CafeQueue
+    {
+        Queue<Person> persons;
+
+        public CafeQueue()
+        {
+            persons = new Queue<Person>();
+        }
+        public void AddToQueue(Person p)
+        {
+            if (p.Time != null) {
+                Console.WriteLine("Посетитель идет за столик по брони");
+            }
+            else
+            {
+                persons.Enqueue(p);
+            }
+           
+        }
+        public void FreeTable()
+        {
+
+            persons.Dequeue();
+            Console.WriteLine("Посетитель идет за столик");
+
+        }
+
+        public void ShowQueue()
+        {
+            foreach(var person in persons)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+    }
+}
